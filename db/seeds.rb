84bb9319385929f80438d36777429f67b5a18032
Diana Rybikov <diana.ryb214@gmail.com>
2021-10-05 19:47:37 -0600

    more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,4 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Flower.create(name: 'spring', image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/closeup-jpg-1614830517.jpg', cost: '$250')
 Flower.create(name: 'summer', image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/christin-geall-1556158724.jpeg?crop=1xw:1xh;center,top&resize=480:*', cost: '$250')
+Flower.create(name: 'fall', image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/fall-flower-arrangement-moon-canyon-veranda-1562778296.jpeg', cost: '$250')
