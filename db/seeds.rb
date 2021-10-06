f26d08622cbd853866302e3fbdb8280d008dd9bf
Diana Rybikov <diana.ryb214@gmail.com>
2021-10-05 19:48:11 -0600

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,4 @@
 Flower.create(name: 'spring', image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/closeup-jpg-1614830517.jpg', cost: '$250')
 Flower.create(name: 'summer', image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/christin-geall-1556158724.jpeg?crop=1xw:1xh;center,top&resize=480:*', cost: '$250')
 Flower.create(name: 'fall', image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/fall-flower-arrangement-moon-canyon-veranda-1562778296.jpeg', cost: '$250')
+Flower.create(name: 'winter', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFfuXS-WcVrkN2JHZ3Hoi_u-zuLa5nHICMnA&usqp=CAU', cost: '$250')
