625f8b2b3cd9a64275c65153480d5b52aa10c0ea
Diana Rybikov <diana.ryb214@gmail.com>
2021-10-06 21:52:07 -0600

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,4 @@ Flower.create(name: 'fall', image: 'https://hips.hearstapps.com/hmg-prod.s3.amaz
 Flower.create(name: 'winter', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFfuXS-WcVrkN2JHZ3Hoi_u-zuLa5nHICMnA&usqp=CAU', cost: '$250')
 Flower.create(name: 'birthday', image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/pink-spring-diy-flower-arrangement-1556650367.jpg', cost: '$250')
 Flower.create(name: 'anniversary', image: 'https://flowermag.com/wp-content/uploads/2017/04/winward1-989x1000.jpg', cost: '$250')
+Flower.create(name: 'roses', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrncVM77Uns589w87BaYquXv5Dnhf8eBlEyg&usqp=CAU', cost: '$250')
